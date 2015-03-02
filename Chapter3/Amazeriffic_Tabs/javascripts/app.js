8f957d1c209ebd950fe5ccbef73c69f601699f32
Francisco Rivas <frankie7413@gmail.com>
2015-03-01 20:22:29 -0800

    added some javacript lines to test tabs in index.html

---
+++ Chapter3/Amazeriffic_Tabs/javascripts/app.js
@@ -0,0 +1,25 @@
+var main = function () {
+   "use strict" ;
+     $(".tabs a:nth-child(1)" ). on("click" , function () {
+     // make all the tabs inactive
+     $(".tabs span" ). removeClass("active" );
+     // make the first tab active
+     $(".tabs a:nth-child(1) span" ). addClass("active" );
+     // empty the main content so we can recreate it
+     $("main .content" ). empty();
+     // return false so we don't follow the link
+     return false;
+});
+   $(".tabs a:nth-child(2)" ). on("click" , function () {
+       $(".tabs span" ). removeClass("active" );
+       $(".tabs a:nth-child(2) span" ). addClass("active" );
+       $("main .content" ). empty();
+       return false;
+   });
+   $(".tabs a:nth-child(3)" ). on("click" , function () {
+       $(".tabs span" ). removeClass("active" );
+       $(".tabs a:nth-child(2) span" ). addClass("active" );
+       $("main .content" ). empty();
+       return false;
+   });
+};
