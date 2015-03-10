749e66c58404bfd28642fe9e8841ad58ff2d3924
Frankie7413 <frankie7413@gmail.com>
2015-03-09 17:26:10 -0700

    added button to javascript to show api workingn in html

--- Chapter5/API_Example/Javascripts/app.js
+++ Chapter5/API_Example/Javascripts/app.js
@@ -1,8 +1,12 @@
 var main = function () {
 	"use strict";
 
-	$.ajax({
-		url: 'http://api.themoviedb.org/3/movie/68734?api_key=5760c5d7185a5ec3493b63d13e65394c',
+	var $button = $("<button>").text("Check Api");
+	$('main .result').append($button);
+
+	$button.on("click", function () {
+		$.ajax({
+		url: 'http://api.themoviedb.org/3/search/movie?api_key=5760c5d7185a5ec3493b63d13e65394c&query=frozen',
 		data: {
 			format: 'json'
 		},
@@ -11,10 +15,29 @@ var main = function () {
 		},
 		dataType: 'jsonp',
 		success: function(data) {
-			$('main .result').append('<p>'+ data.overview +'<p>');
+			//$('main .result').append('<p>'+ data.overview +'</p>');
+			findMovie(data.results[0].id);
 		},
 		type: 'Get'
+		});	
 	});
+
+	function findMovie (id) {
+		$.ajax({
+		url: 'http://api.themoviedb.org/3/movie/' + id + '?api_key=5760c5d7185a5ec3493b63d13e65394c',
+		data: {
+			format: 'json'
+		},
+		error: function() {
+			$('main .result').append('<p>error in ajax</p>');
+		},
+		dataType: 'jsonp',
+		success: function(data) {
+			$('main .result').append('<p>'+ data.overview +'</p>');
+		},
+		type: 'Get'
+		});	
+	}
 
 };
 $(document).ready(main);
